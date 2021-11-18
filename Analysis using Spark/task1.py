4d80e1f1d8c6c877686e397aea386c72534e3b63
Kedar U Shet <62643927+kedarshet@users.noreply.github.com>
2021-11-18 22:45:21 +0530

    Added comments

--- Analysis using Spark/task1.py
+++ Analysis using Spark/task1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+#importing modules
 from pyspark import SparkContext
 from pyspark.sql import *
 from pyspark.sql.functions import col
@@ -8,11 +9,14 @@ import sys
 spark_context = SparkContext.getOrCreate()
 sql_context = SQLContext(spark_context)
 
+#taking command line arguements as input
 country = sys.argv[1].strip()
 city_file = sys.argv[2].strip()
 
+#reading csv file
 data = sql_context.read.csv(city_file,header=True)
 data = data.cache()
+#typecasting
 data = data.withColumn("AverageTemperature",data.AverageTemperature.cast("double"))
 data = data.filter(data.Country==country).select('City',"AverageTemperature")
 data_avg = data.groupby("City").avg("AverageTemperature")
@@ -21,5 +25,6 @@ data_avg = data_avg.withColumnRenamed("City","City2")
 data = data.join(data_avg,data.City ==  data_avg.City2,"inner")
 data = data.filter(data.AverageTemperature > data.avgTemp_allTym)
 data = data.groupby('City').count().sort(col("City")).collect()
+#printing out collected table in required format.
 for i in data:
     print(str(i[0])+'\t'+str(i[1]))
